07ec6337f2c3a6e4cb53c671c73452f8b0590a10
Prabal Singh <prabalsinghup702004@gmail.com>
2026-07-18 13:43:34 +0530

    addRefrenceAccessToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
 router.route("/register").post(
@@ -14,8 +20,13 @@ router.route("/register").post(
       maxCount: 1,
     },
   ]),
-
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
